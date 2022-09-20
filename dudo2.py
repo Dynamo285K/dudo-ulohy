5b0b513a54ee236d5023eaafbacd9275db0c5ce8
Dynamo <tomasolbert0412@gmail.com>
2022-09-20 22:56:08 +0200

    dudo 2 rdy

---
+++ dudo2.py
@@ -0,0 +1,14 @@
+import math
+a=input("zadaj koeficient")
+b=input("zadaj koeficient")
+x=input("zadaj koeficient")
+#skuska
+
+a=int(a)
+b=int(b)
+x=int(x)
+
+if a<=x and x<=b:
+  print("patrí")
+else:
+  print("nepatrí")
